53d42fa0ff4c7e8bcabf469c0e1ce09208c05ba5
kidominox <528774315@qq.com>
2024-06-19 00:29:33 +0800

    dec_inc

---
+++ deepLearning/dec_inc/modelStruct.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+# from layers.RevIN import RevIN
+from transformerStruct import PositionalEmbedding,EnDecoder,Encoder,EmbedDecoder,ConvBlock,AUCNN,AUTransformer
+
+    
+class Convformer(nn.Module):
+    """
+    Vanilla Transformer with O(L^2) complexity
+    """
+    def __init__(self, conv_dropout:float=0., dec_dropout:float=0.,enc_dropout:float=0.,e_layers:int=1,
+                   d_layers:int=1, pe:str='zeros', n_heads:int=16, learn_pe:bool=True,
+                   d_ff:int=512,d_model:int=128, au_len:int=7,imu_len:int=7, 
+                   c_out:int=7,attn_dropout:float=0.,pos_e:str='encoder',au_cor_matrix=None,
+                   is_au_cor:bool=False,au_dropout:float=0.):
+        super(Convformer, self).__init__()
+        # self.revin = revin
+        # if self.revin: self.revin_layer = RevIN(c_in, affine=affine, subtract_last=subtract_last)
+        
+        # conv_block
+        self.conv_block = ConvBlock(window=imu_len,dropout=conv_dropout,channel=106)
+        self.fc2 = nn.Linear(106, c_out)  
+        self.leaky_relu = nn.LeakyReLU()
+
+        # decoder
+        if pos_e == 'encoder':
+            self.decoder = EnDecoder(in_channel=c_out,sq_len=au_len,d_model=d_model,d_layers=d_layers,dropout=dec_dropout,n_heads=n_heads,d_ff=d_ff)
+        elif pos_e == 'embed':
+            self.decoder = EmbedDecoder(c_out=c_out,d_model=d_model,d_layers=d_layers,dropout=dec_dropout,n_heads=n_heads,d_ff=d_ff)
+        self.projection = nn.Linear(d_model, c_out, bias=True)
+
+        # encoder
+
+        self.encoder = Encoder(in_channel=106, dropout=enc_dropout, sq_len=au_len, n_layers=e_layers,d_model=d_model,n_heads=n_heads,
+                        d_ff=d_ff)
+        # au_cor
+        self.au_cor_matrix = au_cor_matrix # au_num * au_num
+        self.is_au_cor = is_au_cor
+        self.au_fc = nn.Linear(c_out, d_model)
+        self.au_leaky_relu = nn.LeakyReLU()
+    
+        # au_cor
+        self.au_cnn = AUCNN(c_out=c_out, d_model=d_model)
+        self.au_tf = AUTransformer(dropout=au_dropout,input_dim=c_out, d_model=256, nhead=n_heads,num_encoder_layers=3,dim_feedforward=1024)
+
+    def forward(self, x, dec_in, au_cor=None):
+        features = []
+        #  x:[batch, au_len60, imu_len200, channels]
+        x = x.permute(0,1,3,2)
+        for i in range(x.shape[1]):
+            features.append(self.conv_block(x[:,i,:,:])) # [batch, imu_len, channels]
+        features_stacked = torch.stack(features, dim=0) #  [au_len, batch, channels]
+
+        # branch 
+        features_bran = torch.reshape(features_stacked, (features_stacked.shape[0]* features_stacked.shape[1],features_stacked.shape[2]))
+
+        features_bran = features_bran.view(features_bran.size(0), -1)  # 这里的-1表示自动计算展平后的特征数量
+        # 通过全连接层 + 激活函数 + Dropout
+        output1 = self.au_leaky_relu(self.fc2(features_bran))
+
+        # decoder
+        x_enc = features_stacked.permute(1,0,2) #  [ batch, au,  channels]
+
+        # x_enc ->  [ batch, au,  106] convblocks
+        # dec_in -> [ batch, au,  au_channel]
+
+        enc_outputs, enc_self_attns = self.encoder(x_enc)
+        dec_outputs, dec_self_attns, dec_enc_attns = self.decoder(dec_in, enc_outputs)
+
+        output2 = self.projection(dec_outputs) # x: [bs  x seq_len x nvars]
+
+        return output1, output2, dec_self_attns, dec_enc_attns
---
+++ deepLearning/dec_inc/train.sh
@@ -0,0 +1,82 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+# if [ ! -d "./logs/Terminal" ]; then
+#     mkdir ./logs/Terminal
+# fi
+
+dataset_path=../dataset_P14_cross/
+data_path=../data_P8_ex/
+patience=10
+stride=4
+patch_len=20
+revin=0 # false 0 true 1
+imu_len=200 # imu desired_length
+au_len=60 #au
+c_in=12 # 没用
+c_out=14
+d_model=256
+n_heads=8
+e_layers=6
+d_layers=6
+d_ff=512
+tf_dropout=0.1
+conv_dropout=0.5
+user=cyx
+num_worker=8
+train_epochs=200
+batch_size=32
+learning_rate=0.01
+dataset_type=auex
+total_people=14
+pred_len=45
+user_mode=cross
+spilt_mode=cut
+time_run=04251623
+lambda1=0.5
+lambda2=1.5
+cuda="1"
+devices="0,1"
+pos_e=encoder
+# is-generate
+# for user in wzf yty yzh lls
+# do
+python -u exp_main.py \
+    --dataset_path $dataset_path \
+    --data_path $data_path\
+    --patch_len $patch_len\
+    --stride $stride\
+    --revin $revin\
+    --imu_len $imu_len\
+    --au_len $au_len\
+    --c_in $c_in\
+    --c_out $c_out\
+    --d_model $d_model\
+    --n_heads $n_heads\
+    --e_layers $e_layers\
+    --d_layers $d_layers\
+    --d_ff $d_ff\
+    --conv_dropout $conv_dropout\
+    --tf_dropout $tf_dropout\
+    --train_epochs $train_epochs\
+    --batch_size $batch_size\
+    --patience $patience\
+    --num_worker $num_worker\
+    --learning_rate $learning_rate\
+    --user_mode $user_mode\
+    --spilt_mode $spilt_mode\
+    --time_run $time_run\
+    --total_people $total_people\
+    --cuda $cuda\
+    --lambda1 $lambda1\
+    --lambda2 $lambda2\
+    --pos_e $pos_e\
+    --user $user\
+    --au_cor False\
+    --is_generate False\
+    --use_multi_gpu False\
+    --pred_len $pred_len\
+    --devices $devices\
+    --dataset_type $dataset_type >logs/$time_run'_'$user'_pred'$pred_len'_cdo'$conv_dropout'_fdo'$tf_dropout'_'$lambda1'_'$lambda2'_im'$imu_len'_au'$au_len'_ci'$c_in'_co'$c_out'_lr'$learning_rate'_pl'$patch_len'_s'$stride'_re'$revin'_dm'$d_model'_nh'$n_heads'_el'$e_layers'_dl'$d_layers'_dff'$d_ff'_'$user_mode'_'$spilt_mode'_'$batch_size'_'$dataset_type.log 
+# done
